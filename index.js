d2629377642de79848e135965e2790b51231e5c4
Angel Alanis <a4alaniz@gmail.com>
2019-07-21 20:55:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
    return newArr;
  }
 
- var facts = [
+ const facts = [
  "He was the last Beatle to learn to drive",
  "He was never a vegetarian",
  "He was a choir boy and boy scout",
